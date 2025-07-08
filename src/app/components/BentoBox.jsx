2807c90026956a73519c4a72251bdac5abc2c738
Kane-Pickett <kane.pickett1@gmail.com>
2025-07-08 20:13:10 +1200

    Added flex-1 and fixed css grid

--- src/app/components/BentoBox.jsx
+++ src/app/components/BentoBox.jsx
@@ -3,9 +3,9 @@
 export default function BentoBox({ children, className }) {
   return (
     <div
-      className={`p-[1px] bg-gradient-to-br from-white/15 via-white/2 to-transparent backdrop-blur-lg rounded-xl group duration-600 hover:scale-105 ${className} motion-preset-rebound-up motion-duration-1000`}
+      className={`flex-1 p-[1px] bg-gradient-to-br from-white/15 via-white/2 to-transparent backdrop-blur-lg rounded-xl group duration-600 hover:scale-105 ${className} motion-preset-rebound-up motion-duration-1000`}
     >
-      <div className="flex justify-between items-center px-4 py-2 bg-gradient-to-br from-black/90 via-black/60 to-transparent backdrop-blur-lg rounded-xl">
+      <div className="flex-1 justify-between items-center px-4 py-2 bg-gradient-to-br from-black/90 via-black/60 to-transparent backdrop-blur-lg rounded-xl">
         {children}
       </div>
     </div>
--- src/app/page.js
+++ src/app/page.js
@@ -16,8 +16,8 @@ export default function Home() {
           id="grid"
           className="absolute inset-0 -z-10 h-full w-full bg-black bg-[linear-gradient(to_right,#181818_1px,transparent_1px),linear-gradient(to_bottom,#181818_1px,transparent_1px)] bg-[size:8rem_8rem] motion-preset-focus motion-duration-2000"
         />
-        <div className="grid grid-cols-5 gap-6 items-stretch">
-          <div className="col-span-1 flex flex-col gap-6 h-full items-stretch">
+        <div className="grid grid-cols-5 gap-6 items-stretch h-full">
+          <div className="col-span-1 flex flex-col gap-6 h-full">
             <BentoBox className="hover:from-amber-400/30 hover:via-amber-400/4 hover:to-transparent motion-delay-400">
               <CurrentLearning />
             </BentoBox>
@@ -35,7 +35,7 @@ export default function Home() {
             </BentoBox>
           </div>
 
-          <div className="col-span-2 flex flex-col gap-6 h-full items-stretch">
+          <div className="col-span-2 flex flex-col gap-6 h-full">
             <BentoBox className="hover:from-violet-400/40 hover:via-violet-400/4 hover:to-transparent motion-delay-400">
               <About />
             </BentoBox>
@@ -45,7 +45,7 @@ export default function Home() {
             </BentoBox>
           </div>
 
-          <div className="col-span-2 flex flex-col gap-6 h-full items-stretch">
+          <div className="col-span-2 flex flex-col gap-6 h-full">
             <BentoBox className="hover:from-emerald-400/30 hover:via-emerald-400/4 hover:to-transparent motion-delay-800">
               <Projects />
             </BentoBox>
